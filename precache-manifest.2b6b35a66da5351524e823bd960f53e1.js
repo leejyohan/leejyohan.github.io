ea528406d0c9b177b2658a2cdc9246ea9be013c4
leejyohan <86225337+leejyohan@users.noreply.github.com>
2022-01-26 14:20:26 +0900

    Add files via upload

---
+++ precache-manifest.2b6b35a66da5351524e823bd960f53e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3637b79d30ad9cc76048",
+    "url": "/css/app.f9b1bc64.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b4e040d7fd1745bab7d2f67fb410579c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3637b79d30ad9cc76048",
+    "url": "/js/app.516f010a.js"
+  },
+  {
+    "revision": "324aad52175090300b0e",
+    "url": "/js/chunk-vendors.ddcf6ab9.js"
+  },
+  {
+    "revision": "7a48e533dbca2832e097778c0f48b6a6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
